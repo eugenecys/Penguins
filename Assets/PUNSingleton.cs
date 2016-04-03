1927032e1aa3141a767de4921739b07a874f15d5
Eugene Chiong <eugenecys@gmail.com>
2016-04-02 23:28:09 -0400

    Added PUNSingleton class for Singletons using the Photon Network

---
+++ Assets/PUNSingleton.cs
@@ -0,0 +1,35 @@
+﻿using Photon;
+using UnityEngine;
+
+public class PUNSingleton<T> : Photon.PunBehaviour where T : Photon.PunBehaviour
+{
+    private static T _instance;
+    private static object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    // Looks for an instance of T in the Object space
+                    // If found, that instance is used.
+                    // Otherwise, it will create a new instance.
+                    _instance = (T)FindObjectOfType(typeof(T));
+                    
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton) " + typeof(T).ToString();
+                        DontDestroyOnLoad(singleton);
+                    }
+                     
+                }
+                return _instance;
+            }
+        }
+    }
+}
