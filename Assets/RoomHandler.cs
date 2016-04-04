3c2ee035274a2a47fa08cbcbe75e9c15d0d30d95
Eugene Chiong <eugenecys@gmail.com>
2016-04-04 01:09:12 -0400

    Added room handler for buttons

---
+++ Assets/RoomHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RoomHandler : MonoBehaviour {
+
+    NetworkMenuManager networkMenuManager;
+    public Text textObj;
+
+    public void join()
+    {
+        networkMenuManager.join(textObj.text);
+    }
+    
+    void Awake()
+    {
+        networkMenuManager = NetworkMenuManager.Instance;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
