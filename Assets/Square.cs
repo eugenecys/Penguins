3f2d540077dce34ca5e0ce804e6f0d4e4d5fff73
Eugene Chiong <eugenecys@gmail.com>
2016-04-04 16:56:39 -0400

    added highlight code for square

--- Assets/Square.cs
+++ Assets/Square.cs
@@ -12,7 +12,9 @@ public class Square : MonoBehaviour {
         IceInactive,
         EmptyInactive,
         IceFading,
-        EmptyFading
+        EmptyFading,
+        IceHighlight,
+        EmptyHighlight
     }
 
     public bool iced
@@ -31,10 +33,29 @@ public class Square : MonoBehaviour {
         }
     }
 
+    public bool isIce
+    {
+        get
+        {
+            return state.Equals(State.Ice) || state.Equals(State.IceHighlight) || state.Equals(State.IceFading) || state.Equals(State.IceInactive);
+        }
+    }
+
+    public bool isEmpty
+    {
+        get
+        {
+            return state.Equals(State.Empty) || state.Equals(State.EmptyHighlight) || state.Equals(State.EmptyFading) || state.Equals(State.EmptyInactive);
+        }
+    }
+
     public GameObject iceSprite;
     public GameObject dirtSprite;
     public GameObject iceInactiveSprite;
     public GameObject dirtInactiveSprite;
+
+    public GameObject dirtHighlightSprite;
+    public GameObject iceHighlightSprite;
 
     public GameObject[] dirtFadeSprites;
     public GameObject[] iceFadeSprites;
@@ -54,7 +75,7 @@ public class Square : MonoBehaviour {
 
     public void deactivate()
     {
-        if (state.Equals(State.Ice) || state.Equals(State.IceInactive) || state.Equals(State.IceFading))
+        if (isIce)
         {
             setState(State.IceInactive);
         }
@@ -66,7 +87,7 @@ public class Square : MonoBehaviour {
 
     public void activate()
     {
-        if (state.Equals(State.Ice) || state.Equals(State.IceInactive) || state.Equals(State.IceFading))
+        if (isIce)
         {
             setState(State.Ice);
         }
@@ -78,76 +99,42 @@ public class Square : MonoBehaviour {
 
     public void setState(State _state)
     {
+        iceSprite.SetActive(false);
+        dirtSprite.SetActive(false);
+        iceInactiveSprite.SetActive(false);
+        dirtInactiveSprite.SetActive(false);
+        for (int i = 0; i < dirtFadeSprites.Length; i++)
+        {
+            dirtFadeSprites[i].SetActive(false);
+        }
+        for (int i = 0; i < iceFadeSprites.Length; i++)
+        {
+            iceFadeSprites[i].SetActive(false);
+        }
+        dirtHighlightSprite.SetActive(false);
+        iceHighlightSprite.SetActive(false);
         switch (_state)
         {
             case State.Empty:
                 state = State.Empty;
-                fadeNum = 0;
-                iceSprite.SetActive(false);
                 dirtSprite.SetActive(true);
-                iceInactiveSprite.SetActive(false);
-                dirtInactiveSprite.SetActive(false);
-                for(int i = 0; i < dirtFadeSprites.Length;i++)
-                {
-                    dirtFadeSprites[i].SetActive(false);
-                }
-                for(int i = 0; i < iceFadeSprites.Length; i++)
-                {
-                    iceFadeSprites[i].SetActive(false);
-                }
+                fadeNum = 0;
                 break;
             case State.Ice:
                 state = State.Ice;
-                fadeNum = 0;
                 iceSprite.SetActive(true);
-                dirtSprite.SetActive(false);
-                iceInactiveSprite.SetActive(false);
-                dirtInactiveSprite.SetActive(false);
-                for (int i = 0; i < dirtFadeSprites.Length; i++)
-                {
-                    dirtFadeSprites[i].SetActive(false);
-                }
-                for (int i = 0; i < iceFadeSprites.Length; i++)
-                {
-                    iceFadeSprites[i].SetActive(false);
-                }
+                fadeNum = 0;
                 break;
             case State.EmptyInactive:
                 state = State.EmptyInactive;
-                iceSprite.SetActive(false);
-                dirtSprite.SetActive(false);
-                iceInactiveSprite.SetActive(false);
                 dirtInactiveSprite.SetActive(true);
-                for (int i = 0; i < dirtFadeSprites.Length; i++)
-                {
-                    dirtFadeSprites[i].SetActive(false);
-                }
-                for (int i = 0; i < iceFadeSprites.Length; i++)
-                {
-                    iceFadeSprites[i].SetActive(false);
-                }
                 break;
             case State.IceInactive:
                 state = State.IceInactive;
-                iceSprite.SetActive(false);
-                dirtSprite.SetActive(false);
                 iceInactiveSprite.SetActive(true);
-                dirtInactiveSprite.SetActive(false);
-                for (int i = 0; i < dirtFadeSprites.Length; i++)
-                {
-                    dirtFadeSprites[i].SetActive(false);
-                }
-                for (int i = 0; i < iceFadeSprites.Length; i++)
-                {
-                    iceFadeSprites[i].SetActive(false);
-                }
                 break;
             case State.EmptyFading:
                 state = State.EmptyFading;
-                iceSprite.SetActive(false);
-                dirtSprite.SetActive(false);
-                iceInactiveSprite.SetActive(false);
-                dirtInactiveSprite.SetActive(false);
                 if (fadeNum >= dirtFadeSprites.Length)
                 {
                     deactivate();
@@ -170,10 +157,6 @@ public class Square : MonoBehaviour {
                 break;
             case State.IceFading:
                 state = State.IceFading;
-                iceSprite.SetActive(false);
-                dirtSprite.SetActive(false);
-                iceInactiveSprite.SetActive(false);
-                dirtInactiveSprite.SetActive(false);
                 if (fadeNum >= iceFadeSprites.Length)
                 {
                     deactivate();
@@ -194,17 +177,52 @@ public class Square : MonoBehaviour {
                     }
                 }
                 break;
+            case State.EmptyHighlight:
+                state = State.EmptyHighlight;
+                dirtHighlightSprite.SetActive(true);
+                break;
+            case State.IceHighlight:
+                state = State.IceHighlight;
+                iceHighlightSprite.SetActive(true);
+                break;
         }
     }
     
+    public void unhighlight()
+    {
+        if (state.Equals(State.IceHighlight))
+        {
+            setState(State.Ice);
+        }
+        else if (state.Equals(State.EmptyHighlight))
+        {
+            setState(State.Empty);
+        }
+    }
+
+    public void highlight()
+    {
+        if (isIce)
+        {
+            setState(State.IceHighlight);
+        } else
+        {
+            setState(State.EmptyHighlight);
+        }
+    }
+
     public void fade()
     {
         if (state.Equals(State.Empty) || state.Equals(State.EmptyFading))
         {
             setState(State.EmptyFading);
         }
+        else if (state.Equals(State.Ice) || state.Equals(State.IceFading)) 
+        {
+            setState(State.IceFading);
+        }
     }
-
+    
 	// Update is called once per frame
 	void Update () {
 	
